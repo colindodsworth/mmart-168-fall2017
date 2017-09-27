3015f101040b3de8d8ca552a644c3da09e6428ca
d <makefunthings@gmail.com>
2017-09-26 21:40:02 -0700

    Homework 4 updates

--- hw04/scripts/part1.js
+++ hw04/scripts/part1.js
@@ -4,11 +4,19 @@ let languageCode
 
 const setLanguage = (code) => {
     //Note: language codes here: https://www.w3schools.com/tags/ref_language_codes.asp
+
+    // this displays the name of the language to be used, based on the language code represented:
     languageCode = code
     if (code === 'ru') {
         language = 'Russian'
     } else if (code === 'es') {
         language = 'Spanish'
+    } else if (code === 'am') {
+        language = 'Amharic'
+    } else if (code === 'ba') {
+        language = 'Bashkir'
+    } else if (code === 'be') {
+        language = 'Belarusian'
     } else {
         language = 'English'
     }
