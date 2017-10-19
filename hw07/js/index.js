dac148622ff7462b2e54cdfdadbad29928c66fde
d <makefunthings@gmail.com>
2017-10-19 01:02:31 -0700

    Complete homework 7

--- hw07/js/index.js
+++ hw07/js/index.js
@@ -8,6 +8,22 @@
 // list ("ul" tag):
 
 
+const appendage = (message) => {    // add li tag to ul tag
+  // simpler bad way:
+    // document.getElementById('output').innerHTML += "<li>" + message + "</li>"
+
+  // better way:
+    const ul = document.getElementById('output')
+    const li = document.createElement('li')
+    const textNode = document.createTextNode(message)
+    li.appendChild(textNode)
+    ul.appendChild(li)
+}
+
+// appendage("hillary, hi there.")
+// appendage("hello, take off your shoes")
+
+
 
 //------------------------------------------------------------------
 // PART II
@@ -19,3 +35,16 @@
 // 2. If the number is divisible by 5, append "Buzz" to the unordered list
 // 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
 // 4. Bonus points: Include both the number *and* the word you've appended to the list
+
+for (let i = 1; i <101; i++) {
+  console.log(i)
+  if (i % 3 === 0 && i % 5 === 0) {
+    appendage('FizzBuzz: ' + i)
+  } else if (i % 3 === 0) {
+    appendage('Fizz: ' + i)
+  } else if (i % 5 === 0) {
+    appendage('Buzz: ' + i)
+  } else {
+    appendage(i)
+  }
+}
